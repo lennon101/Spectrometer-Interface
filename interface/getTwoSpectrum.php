ccff93accb0b0740dec6cb8197cb989ff5dc4c69
Dane Lennon <dane.lennon101@gmail.com>
2016-08-01 16:57:29 +1000

    updating content

--- interface/getTwoSpectrum.php
+++ interface/getTwoSpectrum.php
@@ -1,7 +1,14 @@
 <?php
 include '/home/pi/dev-kit/php/_getspectrum.php';
 
-//echo getspectrum(0);
+//get the date/time
+if (isset($_GET['datetime'])){
+	$datetime = 'datetime';
+} else {
+	$datetime = "could not be found";
+}
+
+echo ("todays datetime: $datetime \n");
 
 //get string of spectrum returned from the supplied php file
 $spectrum0 = getspectrum(0);
@@ -23,7 +30,10 @@ for ($i = 0; $i < $lengthOfArray; $i++) {
 }
 
 //write the spectrum into a text file
-$myfile = fopen("testfile.txt", "w");
+$myfile = fopen("../../../home/pi/dev-kit/php/testfile2.txt", "w");
+
+//write the date/time:
+//fwrite($myfile, "$today[3] / $today[5] / $today[6] ; $today[2] : $today[1]");
 
 for ($i = 0; $i < $lengthOfArray; $i++) {
     fwrite($myfile, "$array0[$i] "."$array1[$i]"."\n");
